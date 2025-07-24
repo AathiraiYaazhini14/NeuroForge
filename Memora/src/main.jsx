3a7db71da672fad0c8556bcc29a4fa743c434b75
saran612 <freelancexsaran@gmail.com>
2025-07-24 14:00:40 +0530

    test-11

--- Memora/src/main.jsx
+++ Memora/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import 'https://aathiraiyaazhini14.github.io/NeuroForge/Memora/index.css'
-import App from 'https://aathiraiyaazhini14.github.io/NeuroForge/Memora/App.jsx'
+import 'https://aathiraiyaazhini14.github.io/NeuroForge/Memora/src/index.css'
+import App from 'https://aathiraiyaazhini14.github.io/NeuroForge/Memora/src/App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
